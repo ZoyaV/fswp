daf710e18cdd12fb3b52eae0b2c78a7d0349edce
zoya <zoya.v@ya.ru>
2022-05-06 20:00:22 +0800

    add logs

--- utils/predict_on_tdataset.py
+++ utils/predict_on_tdataset.py
@@ -11,22 +11,28 @@ sys.path.append("../model/")
 
 from train_unet import UnetTrainer
 
-convlstm_trainer = UnetTrainer("../fswp_run.yaml")
-convlstm_trainer.load_state_dict()
-preds_val, X_val_batch, y_val_batch, losses, fps = convlstm_trainer.predict_once()
-rmse = torch.sqrt(mse_loss(preds_val,y_val_batch))
-plt.subplot(1,2,1)
-plt.title(f"Predicted diffference ")
+print(" //// //// //// //// ")
+print(torch.__version__)
+print(torch.cuda.is_available())
+print(" //// //// //// //// ")
 
-plt.imshow(preds_val[0,0].cpu())
-plt.colorbar(location='bottom', pad=0.05)
+if True:
+    convlstm_trainer = UnetTrainer("../fswp_run.yaml")
+    convlstm_trainer.load_state_dict()
+    preds_val, X_val_batch, y_val_batch, losses, fps = convlstm_trainer.predict_once()
+    rmse = torch.sqrt(mse_loss(preds_val,y_val_batch))
+    plt.subplot(1,2,1)
+    plt.title(f"Predicted diffference ")
 
-plt.subplot(1,2,2)
-plt.title("Original difference")
-plt.imshow(y_val_batch[0,0].cpu())
-plt.colorbar(location='bottom', pad=0.05)
-plt.tight_layout()
-plt.suptitle(f"Residuals prediction \n on batch RMSE = { round(float(rmse),3)}, total RMSE = {round(float(losses)**2,3)}"
-             f" \n FPS = {int(fps)}", fontsize=14)
+    plt.imshow(preds_val[0,0].cpu())
+    plt.colorbar(location='bottom', pad=0.05)
 
-plt.show()
+    plt.subplot(1,2,2)
+    plt.title("Original difference")
+    plt.imshow(y_val_batch[0,0].cpu())
+    plt.colorbar(location='bottom', pad=0.05)
+    plt.tight_layout()
+    plt.suptitle(f"Residuals prediction \n on batch RMSE = { round(float(rmse),3)}, total RMSE = {round(float(losses)**2,3)}"
+                 f" \n FPS = {int(fps)}", fontsize=14)
+
+    plt.show()
