ea228cd5f6788c2cb17a0d17c1d8ecd6908e588b
zoya <zoya.v@ya.ru>
2022-05-10 13:38:25 +0800

    add argparser

--- utils/predict_on_tdataset.py
+++ utils/predict_on_tdataset.py
@@ -4,10 +4,11 @@ from torch import nn
 from torch.nn.functional import mse_loss
 import sys
 import matplotlib.pyplot as plt
+import argparse
 
-sys.path.append("../model/convlstm/")
-sys.path.append("../model/unet/")
-sys.path.append("../model/")
+sys.path.append("./model/convlstm/")
+sys.path.append("./model/unet/")
+sys.path.append("./model/")
 
 from train_unet import UnetTrainer
 
@@ -16,11 +17,12 @@ print(torch.__version__)
 print(torch.cuda.is_available())
 print(" //// //// //// //// ")
 
-if True:
-    convlstm_trainer = UnetTrainer("../fswp_run.yaml")
+def main(dir):
+    convlstm_trainer = UnetTrainer(dir)
     convlstm_trainer.load_state_dict()
     preds_val, X_val_batch, y_val_batch, losses, fps = convlstm_trainer.predict_once()
     rmse = torch.sqrt(mse_loss(preds_val,y_val_batch))
+    print("Images processed. FPS = %s"%fps)
     plt.subplot(1,2,1)
     plt.title(f"Predicted diffference ")
 
@@ -36,3 +38,11 @@ if True:
                  f" \n FPS = {int(fps)}", fontsize=14)
 
     plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Unet example')
+    parser.add_argument('config_dir', type=str, help='Input dir for config', default='../configs/run_solar_fswp.yaml')
+    args = parser.parse_args()
+   # print(args)
+  #  print(args.config_dir)
+    main(args.config_dir)
